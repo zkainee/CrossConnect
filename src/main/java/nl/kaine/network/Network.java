3c056adc91accc99de6af16fc79bc18b4bdfb5a2
kaine <kainevommeren@gmail.,com>
2022-08-10 18:36:25 +0200

    Added welcome message[NEEDS TO BE REMADE]

--- src/main/java/nl/kaine/network/Network.java
+++ src/main/java/nl/kaine/network/Network.java
@@ -1,5 +1,6 @@
 package nl.kaine.network;
 
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
@@ -14,8 +15,6 @@ import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 public final class Network extends Plugin implements Listener {
-
-    private HashMap<UUID, UUID> recentMessages;
 
     @Override
     public void onEnable() {
@@ -32,18 +31,13 @@ public final class Network extends Plugin implements Listener {
         ProxyServer.getInstance().getPluginManager().registerCommand(this, new FruitCommand());
         ProxyServer.getInstance().getPluginManager().registerListener(this, this);
 
-//        ProxyServer.getInstance().getScheduler().schedule(this, () -> {
-//            System.out.println("NETWORK | SCHEDULAR RAN");
-//        }, 10, 5, TimeUnit.SECONDS);
-
     }
 
+    private HashMap<UUID, UUID> recentMessages;
     public HashMap<UUID, UUID> getRecentMessages() { return recentMessages; }
 
     @EventHandler
     public void onPostLogin(PostLoginEvent e) {
-        e.getPlayer().sendMessage("Welkom");
+        e.getPlayer().sendMessage(ChatColor.GREEN + "Welkom " + e.getPlayer());
     }
-
-
 }
